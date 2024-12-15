d2b5000034d7f2e6aa6241bb1d28144271c2ac04
Prasanna2911 <bpp1757@gmail.com>
2024-12-16 00:24:35 +0530

    first

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <section id="home" className="pt-28">
+      <div className="heading-1 container">
+        <h1 className="text-5xl leading-tight font-semibold lg:text-[55px] lg-leading-[1.15] ">
+          Building Scalable Modern Websites for the Future
+        </h1>
+      </div>
+      <div className="btns"></div>
+    </section>
+  );
+};
+
+export default Home;
